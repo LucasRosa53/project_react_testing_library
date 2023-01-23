1f4cef9d3e1fc436a235c9454238c88c07896ac2
LucasRosa53 <lucasrosa223@hotmail.com>
2023-01-23 18:59:22 -0300

    resolvido requisito 3 com ajuda Lucas Augusto

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const notFound = screen.getByText(/No favorite Pokémon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Teste se apenas são exibidos os Pokémon favoritados.', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [pokemonList[0]] } />);
+    const pokemonFav = screen.getByText(/Pikachu/i);
+    expect(pokemonFav).toBeInTheDocument();
+  });
+});
